d3bc8b7f85c39a6fc19917ccca3fbe802f8f51df
ayauri29 <albayauri.29@gmail.com>
2020-05-15 11:41:22 -0500

    Fix images

--- data/data.js
+++ data/data.js
@@ -7,7 +7,7 @@ const PROJECTS = [{
     "tools": [
         "JAVASCRIPT","HTML5","CSS3"
     ],
-    "image": "../images/project/project-image01.png"
+    "image": "https://user-images.githubusercontent.com/33959688/82073848-78455e80-969f-11ea-886b-e415231f2b51.png"
 },
 {
     "id": "2",
@@ -16,7 +16,7 @@ const PROJECTS = [{
     "title": "MEET AND CODE",
     "description": "Red social para personas interesadas en la tecnología, donde de pueden publicar eventos tecnológicos.",
     "tools": ["JAVASCRIPT","CSS3","HTML5","FIREBASE"],
-    "image": "../images/project/project-image02.png"
+    "image": "https://user-images.githubusercontent.com/33959688/82074029-be9abd80-969f-11ea-8e3a-39b856ddf370.png"
 },
 {
     "id": "3",
@@ -25,7 +25,7 @@ const PROJECTS = [{
     "title": "MD-LINKS",
     "description": "Librería que brinda estadísticas sobre los links que se encuentren dentro de todos los archivos .md de una ruta dada.",
     "tools": ["JAVASCRIPT", "NODEJS"],
-    "image": "../images/project/project-image03.png"
+    "image": "https://user-images.githubusercontent.com/33959688/82074180-fbff4b00-969f-11ea-81a2-4050a457bd55.png"
 },
 {
     "id": "4",
@@ -34,7 +34,7 @@ const PROJECTS = [{
     "title": "LOVE CIPHER",
     "description": "Plataforma permite cifrar y descifrar mensajes de amor con una clave de seguridad, sin temor a ser leídas por terceros.",
     "tools": ["JAVASCRIPT", "HTML5","CSS3"],
-    "image": "../images/project/project-image04.png"
+    "image": "https://user-images.githubusercontent.com/33959688/82074337-3cf75f80-96a0-11ea-9259-5c79ab64bf4c.png"
 },
 {
     "id": "5",
@@ -43,7 +43,7 @@ const PROJECTS = [{
     "title": "MOVIE LATIN",
     "description": "Plataforma que muestra información de películas latinoamericanas y elegir qué ver en base a la puntuación de otros.",
     "tools": ["JAVASCRIPT","HTML5","CSS3"],
-    "image": "../images/project/project-image05.png"
+    "image": "https://user-images.githubusercontent.com/33959688/82074427-5e584b80-96a0-11ea-967f-07964ede273c.png"
 },
 {
     "id": "6",
@@ -61,7 +61,7 @@ const PROJECTS = [{
     "title": "BURGER QUEEN",
     "description": "Plataforma que muestra información el menú de un restaurante y permite añadir los productos  a un carrito de compra.",
     "tools": ["VUE", "PUG","NUXT"],
-    "image": "../images/project/79289056-ed99f580-7e8d-11ea-8d60-336e98d55af7.png"
+    "image": "https://user-images.githubusercontent.com/33959688/82074659-b8591100-96a0-11ea-837e-bd532c38a12a.png"
 },
 {
     "id": "8",
@@ -79,7 +79,7 @@ const PROJECTS = [{
     "title": "TIC-TAC-TOE",
     "description": "Juego tres en raya o tic-tac-toe que permite ver los movimientos hechos.",
     "tools": ["REACT"],
-    "image": "https://user-images.githubusercontent.com/33959688/81972908-f0048200-95e8-11ea-85ca-08009a6cbe6b.png"
+    "image": "https://user-images.githubusercontent.com/33959688/82074772-e2123800-96a0-11ea-947a-6304c8aa815a.png"
 }
 
 ]
